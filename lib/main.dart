b7a66a771cc4a14f64c93b45f0bf22c9929f5c71
José Lucas da Silva <joselucas0303@gmail.com>
2021-12-07 01:08:16 -0300

    ThemeText added

--- lib/main.dart
+++ lib/main.dart
@@ -34,8 +34,38 @@ class MyApp extends StatelessWidget {
       title: 'Pafaze',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: Colors.grey,
+          textTheme: TextTheme(
+            headline1: GoogleFonts.openSans(
+                fontSize: 81, fontWeight: FontWeight.w300, letterSpacing: -1.5),
+            headline2: GoogleFonts.openSans(
+                fontSize: 50, fontWeight: FontWeight.w300, letterSpacing: -0.5),
+            headline3:
+                GoogleFonts.openSans(fontSize: 40, fontWeight: FontWeight.w400),
+            headline4: GoogleFonts.openSans(
+                fontSize: 29, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+            headline5:
+                GoogleFonts.openSans(fontSize: 20, fontWeight: FontWeight.w400),
+            headline6: GoogleFonts.openSans(
+                fontSize: 17, fontWeight: FontWeight.w500, letterSpacing: 0.15),
+            subtitle1: GoogleFonts.openSans(
+                fontSize: 13, fontWeight: FontWeight.w400, letterSpacing: 0.15),
+            subtitle2: GoogleFonts.openSans(
+                fontSize: 12, fontWeight: FontWeight.w500, letterSpacing: 0.1),
+            bodyText1: GoogleFonts.openSans(
+                fontSize: 13, fontWeight: FontWeight.w400, letterSpacing: 0.5),
+            bodyText2: GoogleFonts.openSans(
+                fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+            button: GoogleFonts.openSans(
+                fontSize: 12, fontWeight: FontWeight.w500, letterSpacing: 1.25),
+            caption: GoogleFonts.openSans(
+                fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 0.4),
+            overline: GoogleFonts.openSans(
+                fontSize: 8, fontWeight: FontWeight.w400, letterSpacing: 1.5),
+          ).apply(
+              bodyColor: Colors.white,
+              displayColor: Colors.white,
+              decorationColor: Colors.white)),
       home: const HomePage(),
     );
   }
