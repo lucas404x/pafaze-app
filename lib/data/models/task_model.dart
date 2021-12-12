ceb4369c0f6e64a827fe4783cc6df33d178d22e4
José Lucas da Silva <joselucas0303@gmail.com>
2021-12-11 22:07:06 -0300

    initializing properties with default values and added constructor to AddTaskMoel

--- lib/data/models/task_model.dart
+++ lib/data/models/task_model.dart
@@ -1,27 +1,38 @@
 import 'package:hive/hive.dart';
 
+import 'add_task_model.dart';
+
 part 'task_model.g.dart';
 
 @HiveType(typeId: 0)
 class TaskModel {
   @HiveField(0)
-  late String id;
+  late String id = '';
 
   @HiveField(1)
-  late String title;
+  late String title = '';
 
   @HiveField(2)
-  late String description;
+  late String description = '';
 
   @HiveField(3)
-  late bool isDone;
+  late bool isDone = false;
 
   @HiveField(4)
-  late DateTime dateCreated;
+  late DateTime dateCreated = DateTime.now().toUtc();
 
   @HiveField(5)
-  late DateTime dateToDelivery;
+  late DateTime dateToDelivery = DateTime.now().toUtc();
 
   @HiveField(6)
-  late bool isToDelivery;
+  late bool isToDelivery = false;
+
+  TaskModel();
+
+  TaskModel.fromAddTaskModel(AddTaskModel model) {
+    title = model.title;
+    description = model.description;
+    dateToDelivery = model.dateToDelivery;
+    isToDelivery = model.isToDelivery;
+  }
 }
